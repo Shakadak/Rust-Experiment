7f9574845243d3d89d816eded7522d6f5d0dbc6f
Shakadak <nathanael.pineau@gmail.com>
2014-12-12 22:54:57 +0100

    Messing around with generic

--- Tree/src/main.rs
+++ Tree/src/main.rs
@@ -2,55 +2,81 @@
 //fn it_works()
 fn main()
 {
-    let mut root = tree::Leaf
+    let mut root = tree::Leaf::new_leaf::<uint>(50u);
+
+    let mut random;
+
+    for _ in range(1u, 11u)
     {
-        greater: None,
-        lesser:  None,
-        data:    "Green Leaf"
-    };
-    println!("{}", root.data);
+        random = std::rand::random::<uint>() % 100u;
+        print!("{}, ", random);
+        root.add_leaf::<uint>(random);
+    }
+    std::io::print("\n");
 
-    let yellow_leaf = box tree::Leaf {greater: None, lesser: None, data: "Yellow Leaf"};
-    let blue_leaf = box tree::Leaf {greater: None, lesser: None, data: "Blue Leaf"};
-    let black_leaf = box tree::Leaf {greater: None, lesser: None, data: "Black Leaf"};
-
-    root.add_leaf(yellow_leaf);
-    root.add_leaf(black_leaf);
-    root.add_leaf(blue_leaf);
+    root.display_sorted_leaves();
+    std::io::print("\n");
 }
 
 //#[allow(dead_code)]
 mod tree
 {
-    pub struct Leaf
+    pub struct Leaf<T>
     {
-        pub greater:    Option<Box<Leaf>>,
-        pub lesser:     Option<Box<Leaf>>,
-        pub data:       &'static str,
+        pub greater:    Option<Box<Leaf<T>>>,
+        pub lesser:     Option<Box<Leaf<T>>>,
+        pub data:       T,
     }
 
-    impl Leaf
+    impl Leaf<uint>
     {
-        pub fn add_leaf(&mut self, green: Box<Leaf>)
+        pub fn new_leaf<uint>(data: uint) -> Box<Leaf<uint>>
         {
-            match  green.data >= self.data
+            box Leaf
+            {
+                greater:    None,
+                lesser:     None,
+                data:       data
+            }
+        }
+
+        pub fn add_leaf<T>(&mut self, data: uint)
+        {
+            match  data >= self.data
             {
                 true    =>
                 {
                     match self.greater
                     {
-                        Some(ref mut trunk) => trunk.add_leaf(green),
-                        None        => self.greater = Some(green),
+                        Some(ref mut branch)    => branch.add_leaf::<T>(data),
+                        None                    => self.greater = Some(Leaf::new_leaf::<T>(data)),
                     }
                 },
                 false   =>
                 {
                     match self.lesser
                     {
-                        Some(ref mut trunk) => trunk.add_leaf(green),
-                        None        => self.lesser = Some(green),
+                        Some(ref mut branch)    => branch.add_leaf::<T>(data),
+                        None                    => self.lesser = Some(Leaf::new_leaf::<T>(data)),
                     }
                 },
+            }
+        }
+
+        pub fn display_sorted_leaves(self)
+        {
+            match self.lesser
+            {
+                Some(branch)    => branch.display_sorted_leaves(),
+                None            => (),
+            }
+
+            print!("{}, ", self.data);
+
+            match self.greater
+            {
+                Some(branch)    => branch.display_sorted_leaves(),
+                None            => (),
             }
         }
     }
