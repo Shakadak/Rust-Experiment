7cd6b0e2393aa8cd4e162d91bfa51b9a0a5d397d
shakadak <nathanael.pineau@gmail.com>
2015-05-23 14:38:38 +0200

    update

--- FizzBuzz/src/main.rs
+++ FizzBuzz/src/main.rs
@@ -1,13 +1,7 @@
-fn main()
-{
-    for n in range(1u, 101u)
-    {
-        match n
-        {
+fn main() {
+    for n in 1..101 {
+        match n {
             n if n % 15 == 0 => println!("FizzBuzz"),
             n if n % 3 == 0 => println!("Fizz"),
             n if n % 5 == 0 => println!("Buzz"),
-            _ => println!("{}", n),
-        }
-    }
-}
+            _ => println!("{}", n), }}}
